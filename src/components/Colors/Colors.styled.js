e4b852c12ca6502ee76225323207168db99278c0
MARiAN-SH <marianwebstudio@gmail.com>
2025-10-05 20:16:11 +0200

    Update Colors.styled.js

--- src/components/Colors/Colors.styled.js
+++ src/components/Colors/Colors.styled.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 export const BlockColors = styled("div")`
     display: flex;
     max-width: 100%;
-    height: 100vh;
+    height: 100dvh;
     justify-content: space-between;
     align-content: center;
     flex-wrap: wrap;
